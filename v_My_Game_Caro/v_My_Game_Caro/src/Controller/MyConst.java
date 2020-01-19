33d6d95f3072b2a0c8116b4cc41c26ca8fcf7c6a
Son-Hust <54366579+Son-Hust@users.noreply.github.com>
2020-01-19 16:02:33 +0700

    Commit nhe

---
+++ v_My_Game_Caro/v_My_Game_Caro/src/Controller/MyConst.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+/**
+ *
+ * @author NguyenTuanVu
+ */
+public interface MyConst {
+
+    public final int HEIGHT_MAX = 20;
+    public final int WIDTH_MAX = 20;
+    //header data gui di
+    public final char HEADER_CHAT = '1';
+    public final char HEADER_NUOC_DI = '2';
+    public final char HEADER_NAME = '3';
+    //state game
+    public final char VS_COMPUTER = 1;
+    public final char VS_PLAYER = 2;
+    public final char STATE_THANG = 1;
+    public final char STATE_THUA = 2;
+    public final char STATE_HOA = 3;
+    public final char STATE_NONE = 4;
+}
