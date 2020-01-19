d486fef0e025eb9da70a60c3f1712f7fd92ffc41
Son-Hust <54366579+Son-Hust@users.noreply.github.com>
2020-01-19 15:53:45 +0700

    Commit Exam1_CRUD

---
+++ CRUD_Example1/src/main/java/com/springboot/crud/crud_hibernate_mysql/CrudHibernateMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.crud.crud_hibernate_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudHibernateMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudHibernateMysqlApplication.class, args);
+    }
+
+}
---
+++ CRUD_Example1/src/test/java/com/springboot/crud/crud_hibernate_mysql/CrudHibernateMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.crud.crud_hibernate_mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudHibernateMysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
